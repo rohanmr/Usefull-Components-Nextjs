0fafe6b37f2328f2bbcc258b2c8fd4accfaff675
Rohan2104 <maindarkarrohan@yahoo.com>
2023-05-24 11:41:17 +0530

    fixing style error

--- app/page.js
+++ app/page.js
@@ -1,6 +1,8 @@
+import Styles from "../app/page.module.css";
+
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main className={Styles.main}>
       <div>
         <h1>Hello World</h1>
       </div>
