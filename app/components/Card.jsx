543f80ade0980c01ce99450cb92822cf1fefd502
Rohan2104 <maindarkarrohan@yahoo.com>
2023-05-27 17:22:20 +0530

    added some style to the components

--- app/components/Card.jsx
+++ app/components/Card.jsx
@@ -3,9 +3,20 @@ import Styles from "../styles/card.module.css";
 
 const Card = () => {
   return (
-    <div>
-      <h1>Card with Some Style</h1>
-    </div>
+    <>
+      <h1>Card With Some Style</h1>
+      <div className={Styles.card}>
+        <img
+          src="https://images.pexels.com/photos/2631613/pexels-photo-2631613.jpeg?auto=compress&cs=tinysrgb&w=600"
+          alt="image with tree"
+          className={Styles.card_img}
+        />
+        <div className={Styles.card_body}>
+          <h2 className={Styles.card_title}>Card Fro Style</h2>
+          <p className={Styles.card_desc}>Taravle is good for the soul</p>
+        </div>
+      </div>
+    </>
   );
 };
 
---
+++ app/components/Navbar.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import Styles from "../styles/navbar.module.css";
+
+const Navbar = () => {
+  return <div>Navbar</div>;
+};
+
+export default Navbar;
