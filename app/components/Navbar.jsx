11de5beb37c2e4765b5cceda0d62aee5ed4dcb32
Rohan2104 <maindarkarrohan@yahoo.com>
2023-05-31 16:43:39 +0530

    navabar added

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,8 +1,34 @@
-import React from "react";
+import Link from "next/link";
 import Styles from "../styles/navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className={Styles.navbar}>
+      <div className={Styles.navbarContainer}>
+        <Link href="/" className={Styles.brand}>
+          Compos
+        </Link>
+        <div className={Styles.links}>
+          <Link href="/home" className={Styles.link}>
+            Home
+          </Link>
+          <Link href="/products" className={Styles.link}>
+            Cards
+          </Link>
+          <Link href="/cart" className={Styles.link}>
+            Buttons
+          </Link>
+          <Link href="/about" className={Styles.link}>
+            Footers
+          </Link>
+        </div>
+        <div className={Styles.authButtons}>
+          <button className={Styles.logoutButton}>Login</button>
+          <button className={Styles.logoutButton}>Logout</button>
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
--- app/page.js
+++ app/page.js
@@ -1,10 +1,14 @@
 import Styles from "../app/page.module.css";
 import Card from "./components/Card";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
-    <main className={Styles.main}>
-      <Card />
-    </main>
+    <>
+      <Navbar />
+      <main className={Styles.main}>
+        <Card />
+      </main>
+    </>
   );
 }
